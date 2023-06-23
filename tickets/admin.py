bf32a74061239eabfcc07a21c37acbc15a3ac8d2
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2023-06-23 17:24:34 +0200

    model inplemented

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from tickets.models import Ticket, Category, Priority, State
+
+admin.site.register(Ticket)
+admin.site.register(Category)
+admin.site.register(Priority)
+admin.site.register(State)
+
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,36 @@
+import datetime
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    def __str__(self):
+        return f'{self.id} {self.name}'
+
+class Priority(models.Model):
+    name = models.CharField(max_length=300)
+    def __str__(self):
+        return f'{self.id} {self.name}'
+
+class State(models.Model):
+    name = models.CharField(max_length=300)
+    def __str__(self):
+        return f'{self.id} {self.name}'
+class Ticket(models.Model):
+    title = models.CharField(max_length=300)
+    category =  models.ForeignKey(Category,on_delete=models.CASCADE)
+    description = models.TextField(blank= True)
+    priority = models.ForeignKey(Priority,on_delete=models.CASCADE)
+    status = models.ForeignKey(State,on_delete=models.CASCADE)
+    maintask = models.BooleanField(default=False)
+    assigned = models.CharField(max_length=100)
+    date = models.BigIntegerField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(default=datetime.date.today)
+    def __str__(self):
+        return f'{self.id} {self.title}'
